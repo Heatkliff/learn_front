fc121e5a0b3925004e87672b04fd82aa48b0ae02
NIkita Koryak <n.koryak@evnedev.com>
2019-08-15 17:57:05 +0300

    #15957 Landing - CSS Education Ready banner

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,4 +2,6 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
+
+import 'slick-carousel'
 require('./main/test.js');
--- assets/scripts/main/test.js
+++ assets/scripts/main/test.js
@@ -1,5 +1,22 @@
 jQuery(
     $(function () {
+        const slider = $(".slider");
 
+        slider.slick({
+            vertical: true,
+            dots: true,
+            arrows: false,
+            dotsClass: "slider-dots",
+            verticalSwiping: true
+        });
+
+        slider.on('wheel', (function(e) {
+            e.preventDefault();
+            if (e.originalEvent.deltaY > 0) {
+                $(this).slick('slickNext');
+            } else {
+                $(this).slick('slickPrev');
+            }
+        }));
     })
 );
